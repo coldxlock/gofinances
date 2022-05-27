b6cfead929d51c38f63ba158ea47246b2b4c2027
priscocleyton <priscocleyton@gmail.com>
2022-05-26 23:12:58 -0300

    Iniando a página Dashboard

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
 
-import { Welcome } from './src/Components/Welcome';
+import { Dashboard } from './src/screens/Dashboard/Dashboard';
 
 export default function App() {
-  return (
-    <Welcome  title='React Native Bare Workflor com Typescript'  />
-  );
+  return <Dashboard/>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
