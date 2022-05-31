3c771fe14e8513dbba77cffa43ba2e57cfadfbce
priscocleyton <priscocleyton@gmail.com>
2022-05-30 22:47:41 -0300

    Adicionando Tema , com fonts e cores

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { 
+  Container, 
+  Title, 
+  SubTitle 
+} from './styles';
+
+export function Dashboard(){
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+    )
+}
